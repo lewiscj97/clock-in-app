3530da93ea5ffab142b97e372ad6aff3f8a1e349
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-16 12:14:30 +0000

    test(*): write tests for creating an account

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,11 +8,16 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    it "redirect to the welcome page" do
+      post "/users", params: {user: {first_name: 'Moggy', last_name: 'Mog', email: 'moggymog@meowmail.com', password: 'fish'}}
+      expect(response).to redirect_to('/welcome')
+    end
+
+    it "creates a new user" do
+      post "/users", params: {user: {first_name: 'Test', last_name: 'Testing', email: 'testing@example.com', password: 'fish'}}
+      user = User.find_by(first_name: 'Test')
+      expect(user.last_name).to eq('Testing')
     end
   end
-
 end
