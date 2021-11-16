cf935c62eb9d2959ad839a7d3a46cc9e8d87d26b
duckduckboom <opowercourse@icloud.com>
2021-11-16 11:48:33 +0000

    Started to add fixtures

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
 
+  fixtures :all 
   describe "GET /create" do
     it "returns http success" do
-      get "/sessions/create"
+      post "/login", params: {user: {email: 'moggymog@meowmail.com', password: 'fish'}}
       expect(response).to have_http_status(:success)
     end
   end
