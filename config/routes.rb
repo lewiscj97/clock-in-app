24e1af73687be5aaac9b9f1d84553e90f85d3e18
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-16 10:55:47 +0000

    feat(*): add user authorization

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
 end
