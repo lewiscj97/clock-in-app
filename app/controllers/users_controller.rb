f82ad93fbb6c7272dcd333146d5f71941d022701
duckduckboom <opowercourse@icloud.com>
2021-11-16 10:29:16 +0000

    feat(*): Added to views and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(params.require(:user).permit(:email, :first_name, :last_name, :password))
+    session[:user_id] = @user.id
+    redirect_to '/welcome'
   end
 end
