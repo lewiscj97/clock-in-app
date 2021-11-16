1079c48641da6881db65824fcd38bc12a6b9411f
duckduckboom <opowercourse@icloud.com>
2021-11-16 16:30:39 +0000

    test(entries_spec): Created an entry test finished and passing

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Entries", type: :request do
-  fixtures :all
+RSpec.describe EntriesController, type: :controller do
+  fixtures :users
 
   describe "creating an entry" do
-    it "should create an enrty" do
-      post "/entries", params: {user_id: '1', entry_type: '0'}
+    it "should create an entry" do
+      allow(controller).to receive(:current_user) {users(:mog)}
+      post :create, params: {entry_type: '0'}
       entry = Entry.all.first
-      expect(entry.user_id).to eq('1')
+      expect(entry.user_id).to eq(1)
+      expect(entry.entry_date).to eq(Date.today)
     end
   end
 
