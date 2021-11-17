84abb4b73a82ee5372680e624b7d2069322695c6
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-17 14:18:18 +0000

    feat(*): add seed values to development database and show values in timesheet table

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -2,13 +2,12 @@ class TimesheetController < ApplicationController
   def index
     entries = current_user.entries
 
+    @entry_types = ['AM Start', 'AM Finish', 'PM Start', 'PM Finish']
+
     @monday = entries.where(entry_date: '2021-11-15')
     @tuesday = entries.where(entry_date: '2021-11-16')
     @wednesday = entries.where(entry_date: '2021-11-17')
     @thursday = entries.where(entry_date: '2021-11-18')
     @friday = entries.where(entry_date: '2021-11-19')
-
-    @week = [@monday, @tuesday, @wednesday, @thursday, @friday]
-    # p @monday.where(entry_type:0).first.entry_time.strftime('%H:%M')
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(id: 1, email: 'moggymog@meowmail.com', first_name: 'Moggy', last_name: 'Mog', password_digest: BCrypt::Password.create('fish'))
+
+Entry.create(user_id: 1, entry_type: 0, entry_date: Date.new(2021, 11, 15), entry_time: Time.new(2021, 11, 15, 9, 0, 0))
+Entry.create(user_id: 1, entry_type: 1, entry_date: Date.new(2021, 11, 15), entry_time: Time.new(2021, 11, 15, 12, 0, 0))
+Entry.create(user_id: 1, entry_type: 2, entry_date: Date.new(2021, 11, 15), entry_time: Time.new(2021, 11, 15, 13, 0, 0))
+Entry.create(user_id: 1, entry_type: 3, entry_date: Date.new(2021, 11, 15), entry_time: Time.new(2021, 11, 15, 17, 0, 0))
+
+Entry.create(user_id: 1, entry_type: 0, entry_date: Date.new(2021, 11, 16), entry_time: Time.new(2021, 11, 16, 9, 0, 0))
+Entry.create(user_id: 1, entry_type: 1, entry_date: Date.new(2021, 11, 16), entry_time: Time.new(2021, 11, 16, 12, 0, 0))
+Entry.create(user_id: 1, entry_type: 2, entry_date: Date.new(2021, 11, 16), entry_time: Time.new(2021, 11, 16, 13, 0, 0))
+Entry.create(user_id: 1, entry_type: 3, entry_date: Date.new(2021, 11, 16), entry_time: Time.new(2021, 11, 16, 17, 0, 0))
+
+Entry.create(user_id: 1, entry_type: 0, entry_date: Date.new(2021, 11, 17), entry_time: Time.new(2021, 11, 17, 9, 0, 0))
+Entry.create(user_id: 1, entry_type: 1, entry_date: Date.new(2021, 11, 17), entry_time: Time.new(2021, 11, 17, 12, 0, 0))
+Entry.create(user_id: 1, entry_type: 2, entry_date: Date.new(2021, 11, 17), entry_time: Time.new(2021, 11, 17, 13, 0, 0))
+Entry.create(user_id: 1, entry_type: 3, entry_date: Date.new(2021, 11, 17), entry_time: Time.new(2021, 11, 17, 17, 0, 0))
+
+Entry.create(user_id: 1, entry_type: 0, entry_date: Date.new(2021, 11, 18), entry_time: Time.new(2021, 11, 18, 9, 0, 0))
+Entry.create(user_id: 1, entry_type: 1, entry_date: Date.new(2021, 11, 18), entry_time: Time.new(2021, 11, 18, 12, 0, 0))
+Entry.create(user_id: 1, entry_type: 2, entry_date: Date.new(2021, 11, 18), entry_time: Time.new(2021, 11, 18, 13, 0, 0))
+Entry.create(user_id: 1, entry_type: 3, entry_date: Date.new(2021, 11, 18), entry_time: Time.new(2021, 11, 18, 17, 0, 0))
+
+Entry.create(user_id: 1, entry_type: 0, entry_date: Date.new(2021, 11, 19), entry_time: Time.new(2021, 11, 19, 9, 0, 0))
+Entry.create(user_id: 1, entry_type: 1, entry_date: Date.new(2021, 11, 19), entry_time: Time.new(2021, 11, 19, 12, 0, 0))
+Entry.create(user_id: 1, entry_type: 2, entry_date: Date.new(2021, 11, 19), entry_time: Time.new(2021, 11, 19, 13, 0, 0))
+Entry.create(user_id: 1, entry_type: 3, entry_date: Date.new(2021, 11, 19), entry_time: Time.new(2021, 11, 19, 17, 0, 0))
