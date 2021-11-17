f1fad4b3f714e0fc4cf9318f40bccd99c4b37df4
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-17 14:21:42 +0000

    test(view_timesheet): add feature test for viewing specific timesheet

--- spec/features/view_timesheet_spec.rb
+++ spec/features/view_timesheet_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 feature 'view timesheets' do
   fixtures :entries
+  
+  before(:each) { login }
+
   scenario 'a user can view timesheets' do
-    login
     visit('/entries')
     click_button('View Timesheets')
     expect(page).to have_content("Monday")
     expect(page).to have_content("09:00")
+  end
+
+  scenario 'A user can view a specific timesheet given the year and week number' do
+    visit('/timesheet/2021/46')
+    expect(page).to have_content('15/11/21')
+    expect(page).to have_content('Monday')
+    expect(page).to have_content('09:00')
   end
 end
