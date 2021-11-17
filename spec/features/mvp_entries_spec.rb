7db69971a452abbc1114e7cc4d02be151eb1f3a2
duckduckboom <opowercourse@icloud.com>
2021-11-17 10:08:47 +0000

    feat(entry_buttons): Finished 4 buttons and feature tests

--- spec/features/mvp_entries_spec.rb
+++ spec/features/mvp_entries_spec.rb
@@ -9,5 +9,33 @@ feature 'Entries' do
     
     entry = Entry.all.first
     expect(entry.entry_date).to eq(Date.today)
+    expect(entry.entry_type).to eq(0)
+  end 
+
+  scenario 'A signed in user can clock in - AM finish' do
+    visit('/entries')
+    click_button('AM Finish')
+    
+    entry = Entry.all.first
+    expect(entry.entry_date).to eq(Date.today)
+    expect(entry.entry_type).to eq(1)
+  end 
+
+  scenario 'A signed in user can clock in - PM start' do
+    visit('/entries')
+    click_button('PM Start')
+    
+    entry = Entry.all.first
+    expect(entry.entry_date).to eq(Date.today)
+    expect(entry.entry_type).to eq(2)
+  end 
+
+  scenario 'A signed in user can clock in - PM Finish' do
+    visit('/entries')
+    click_button('PM Finish')
+    
+    entry = Entry.all.first
+    expect(entry.entry_date).to eq(Date.today)
+    expect(entry.entry_type).to eq(3)
   end 
 end
