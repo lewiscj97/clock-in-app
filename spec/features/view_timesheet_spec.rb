fc6a821134064cc93c92597b300b0db209b23b95
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-17 16:11:49 +0000

    test(view_timesheet): add feature spec for getting specific timesheets through a form

--- spec/features/view_timesheet_spec.rb
+++ spec/features/view_timesheet_spec.rb
@@ -18,9 +18,17 @@ feature 'view timesheets' do
     expect(page).to have_content('09:00')
   end
 
-  scenario 'Use can view specific timesheet that contains no data' do
+  scenario 'User can view specific timesheet that contains no data' do
     visit('/timesheet/2021/45')
     expect(page).to have_content('Monday')
     expect(page).to have_content('-')
+  end
+
+  scenario 'User can select which timesheet they want to view through a form' do
+    visit('/timesheet/2021/45')
+
+    fill_in 'week', with: '2021-W46'
+    click_button('Submit')
+    expect(page).to have_current_path('/timesheet/2021/46')
   end
 end
