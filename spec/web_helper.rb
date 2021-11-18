1ea4d2ba614d77fb0e038db6f5c773644553569a
duckduckboom <opowercourse@icloud.com>
2021-11-18 12:42:32 +0000

    chore(*): Altered buttons and fixed tests

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,17 +1,17 @@
 def sign_up
   visit('/welcome')
-  click_button('Sign Up')
+  click_button('Register')
   fill_in(:user_first_name, with: 'joe')
   fill_in(:user_last_name, with: 'bloggs')
   fill_in(:user_email, with: 'joe.bloggs@123.com')
   fill_in(:user_password, with: 'password')
-  click_button('Create User')
+  click_button('Create Account')
 end
 
 def login
   visit('/welcome')
-  click_button('Login')
+  click_button('Log In')
   fill_in(:email, with: 'moggymog@meowmail.com')
   fill_in(:password, with: 'fish')
-  click_button('Login')
+  click_button('Log In')
 end
