753b9f92d87342b1d18ee2fd0c9619a669ce04ce
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-18 15:01:03 +0000

    feat(*): user can make entries with a single button

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,13 @@
 class EntriesController < ApplicationController
   def create
-    @entry = current_user.entries.create(entry_time: Time.now, entry_date: Date.today, entry_type: params[:entry_type])
+    users_entries_for_the_day = current_user.entries.where(entry_date: Date.today)
+
+    if users_entries_for_the_day.count <= 3
+      @entry = current_user.entries.create(entry_time: Time.now, entry_date: Date.today, entry_type: users_entries_for_the_day.count)
+    else
+      flash[:too_many_entries] = 'Too many entries'
+    end
+
     redirect_to '/entries'
   end
 end
