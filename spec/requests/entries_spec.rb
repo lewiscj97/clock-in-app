70a9a697614c2d44c82cebbdc72282f7051aada4
duckduckboom <opowercourse@icloud.com>
2021-11-18 15:36:48 +0000

    test(entries_spec.rb): fixed entry test, added flash message test

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -8,10 +8,21 @@ RSpec.describe EntriesController, type: :controller do
   describe "creating an entry" do
     it "should create an entry" do
       allow(controller).to receive(:current_user) {users(:mog)}
-      post :create, params: {entry_type: '0'}
+      post :create
       entry = Entry.all.first
       expect(entry.user_id).to eq(1)
       expect(entry.entry_date).to eq(Date.today)
+    end
+
+    it "sets flash message when more than 4 entries attempted" do
+      allow(controller).to receive(:current_user) {users(:mog)}
+      post :create
+      post :create
+      post :create
+      post :create
+      post :create
+      expect(Entry.all.count).to eq(4)
+      expect(flash[:too_many_entries]).to eq("Too many entries")
     end
   end
 end
