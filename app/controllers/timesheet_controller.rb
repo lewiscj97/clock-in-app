f405208e3c9c16c93e6af17d0fcb1b3094840096
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-22 10:11:29 +0000

    feat(*): add link to each entry which directs to the specific entry page

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -1,38 +1,38 @@
 class TimesheetController < ApplicationController
   def show
-    year = params[:year].to_i
-    week_number = params[:week_number].to_i
-    @year_week_string = "#{year}-W#{params[:week_number]}"
+    @year = params[:year].to_i
+    @week_number = params[:week_number].to_i
+    @year_week_string = "#{@year}-W#{params[:week_number]}"
 
     # Collection of dates in 'yyyy-mm-dd' format
-    weekdays = []
+    @weekdays = []
 
     for i in 1..5 do
-      weekdays.push(Date.commercial(year, week_number, i).to_s)
+      @weekdays.push(Date.commercial(@year, @week_number, i).to_s)
     end
 
     entries = current_user.entries
 
     @entry_types = ['AM Start', 'AM Finish', 'PM Start', 'PM Finish']
     @days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    @week_commencing = weekdays.first
+    @week_commencing = @weekdays.first
     
     # Collection of entries for each day
     @weekday_entries = [] 
 
     for i in 0..4 do
-      @weekday_entries.push(entries.where(entry_date: weekdays[i]))
+      @weekday_entries.push(entries.where(entry_date: @weekdays[i]))
     end
 
     respond_to do |format|
       format.html do
         # Stop user from accessing timesheets from before 2021
-        redirect_to('/timesheet/2021/1') and return if year < 2021
+        redirect_to('/timesheet/2021/1') and return if @year < 2021
         render 'show'
       end
       
       format.xlsx do
-        response.headers['Content-Disposition'] = "attachment; filename=#{Date.today}-timesheet-#{year}-#{week_number}.xlsx"
+        response.headers['Content-Disposition'] = "attachment; filename=#{Date.today}-timesheet-#{@year}-#{@week_number}.xlsx"
       end
     end
   end
