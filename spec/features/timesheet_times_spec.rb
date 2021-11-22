888009a128b5c13336347205c23f887c9fa8c57e
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-22 15:02:41 +0000

    test(timesheet_times): add tests for when only am or pm entries filled

--- spec/features/timesheet_times_spec.rb
+++ spec/features/timesheet_times_spec.rb
@@ -11,4 +11,23 @@ feature 'Show user total daily times' do
     visit('/timesheet/2021/46')
     expect(page).to have_content('6:59')
   end
+
+  scenario '0:00 shown when no times registered' do
+    visit('/timesheet/2021/48')
+    expect(page).to have_content('0:00')
+  end
+
+  scenario 'AM totals shown when only AM times filled' do
+    Entry.create(user_id: 1, entry_date: Date.new(2021,11,29), entry_type: 0, entry_time: Time.new(2000,1,1,9,0,0))
+    Entry.create(user_id: 1, entry_date: Date.new(2021,11,29), entry_type: 1, entry_time: Time.new(2000,1,1,12,30,0))
+    visit('/timesheet/2021/48')
+    expect(page).to have_content('3:30')
+  end
+
+  scenario 'PM totals shown when only PM times filled' do
+    Entry.create(user_id: 1, entry_date: Date.new(2021,11,29), entry_type: 2, entry_time: Time.new(2000,1,1,13,0,0))
+    Entry.create(user_id: 1, entry_date: Date.new(2021,11,29), entry_type: 3, entry_time: Time.new(2000,1,1,17,30,0))
+    visit('/timesheet/2021/48')
+    expect(page).to have_content('4:30')
+  end
 end
