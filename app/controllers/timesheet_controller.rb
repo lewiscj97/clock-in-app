62edc2f3753f95ee812aa774a5a9b1306bedb27a
Lewis Jones <lewis.jones1997@protonmail.com>
2021-11-22 11:53:30 +0000

    test(timesheet_spec): add tests for post_edit and patch_edit methods

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -49,11 +49,11 @@ class TimesheetController < ApplicationController
   end
 
   def edit_entry
-    @entry = Entry.where(entry_date: params[:date], entry_type: params[:entry_type]).first_or_create
+    @entry = current_user.entries.where(entry_date: params[:date], entry_type: params[:entry_type]).first_or_create
   end
 
   def patch_edit
-    entry = Entry.where(entry_date: params[:entry_date], entry_type: params[:entry_type]).first
+    entry = current_user.entries.where(entry_date: params[:entry_date], entry_type: params[:entry_type]).first
     entry.entry_time = params.require(:entry)[:entry_time]
     entry.save
     redirect_to("/timesheet/#{params[:year]}/#{params[:week]}")
